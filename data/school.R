8164ba3f48cfae4152af5fe8377869a311e1e189
vamsikrishna97 <pvamsikrishna97@gmail.com>
2016-03-07 21:32:47 +0530

    deleted repeated data

--- data/school.R
+++
@@ -1,7 +0,0 @@
-setwd("/home/vamsi/data")
-institute<-read.csv("institutes.csv")
-library(ggplot2)
-head(institute)
-colnames(institute)<-c("Sl.No","State","Area","Balwadi.institutes","Balwadi.Teachers.M","Balwadi.Teachers.F","Anganwadi.institutes","Anganwadi.Teachers.M","Anganwadi.Teachers.F","ECCE.institutes","ECCE.Teachers.M","ECCE.Teachers.F","Preprimary.institutes","Preprimary.Teachers.M","Preprimary.Teachers.F","Unrecognised.institutes","Unrecognised.Teachers.M","Unrecognised.Teachers.F")
-x<-c(106,107,108)
-institute<-institute[-x,]
